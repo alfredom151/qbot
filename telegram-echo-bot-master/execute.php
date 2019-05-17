8ad1bfcdff269dd9043f76c55c7d3e6544e15213
alfredom151 <50722708+alfredom151@users.noreply.github.com>
2019-05-17 02:24:58 +0200

    Add files via upload

--- telegram-echo-bot-master/execute.php
+++ telegram-echo-bot-master/execute.php
@@ -9,7 +9,7 @@ if(!$update)
 
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = -383890760
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
