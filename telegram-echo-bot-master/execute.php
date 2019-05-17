afd2e7b49f2f4f635bfbcdaae6c6ee8557f7b12f
alfredom151 <50722708+alfredom151@users.noreply.github.com>
2019-05-17 02:44:12 +0200

    Add files via upload

--- telegram-echo-bot-master/execute.php
+++ telegram-echo-bot-master/execute.php
@@ -9,7 +9,7 @@ if(!$update)
 
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$chatId = isset($message['chat’][‘-383890760’]) ? $message['chat’][‘-383890760’] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
@@ -20,6 +20,6 @@ $text = trim($text);
 $text = strtolower($text);
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => “-383890760”, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $text);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
